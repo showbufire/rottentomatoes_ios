90a9492c15b8bdb454d563a5984d935f4dc95e90
Xiao Jiang <showbufire@gmail.com>
2014-10-14 21:57:44 -0700

    Initial Commit

---
+++ rottentomato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottentomato
+//
+//  Created by Xiao Jiang on 10/14/14.
+//  Copyright (c) 2014 Xiao Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottentomato/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rottentomato
+//
+//  Created by Xiao Jiang on 10/14/14.
+//  Copyright (c) 2014 Xiao Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
